eff9d582f018aa268fb5110febc02ee2f82068f3
Minh Phan <minhphan@MacBook-Pro-5.local>
2017-07-27 17:33:14 -0400

    Add display of candidate products in results list

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,14 @@ const Card = ({ consultant }) => {
   <div className="card mt-3">
     <div className="card-block">
       <h4 className="card-title">{consultant.given_name} {consultant.family_name}</h4>
-      <h6 className="card-subtitle mb-2 text-muted">{consultant.locality}</h6>
+      <h5 className="card-subtitle mb-2 text-muted">{consultant.locality}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">
+        { consultant.products.map(item => {
+          return (
+            <span> {item.name}, </span>
+          );
+        })}
+      </h6>
      <a href={consultant.linkedin_uri} className="card-link">LinkedIn</a> 
     </div>
   </div>
